1376d5f981b5962dec0dfb68146bfd122323c106
Beckett Devoe <bld@mit.edu>
2024-01-02 12:17:22 -0500

    test user

--- loadTransaction.js
+++ loadTransaction.js
@@ -1,4 +1,17 @@
 //verify that you can load a file
+function logCurrentUser() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in, log user details
+            console.log("Logged in User:", user);
+        } else {
+            // No user is signed in, log this state
+            console.log("No user currently logged in");
+        }
+    });
+}
+
+
 function fetchUniqueTransactionData(transactionId) {
     return new Promise((resolve, reject) => {
         var transactionRef = firebase.database().ref('/uniqueTransactions/' + transactionId);
@@ -19,5 +32,7 @@ function fetchUniqueTransactionData(transactionId) {
 
 
 //Main script
+logCurrentUser
+
 const testId = "-Nn6w1P77VCQ_51Bz60Z"
 fetchUniqueTransactionData(testId)
